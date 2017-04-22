40d3e24fe3b81ab6e17cddc63f6bf15b4794f3af
Mads Rønnow <roennowmads@gmail.com>
2017-04-22 22:06:37 +0200

    Proof-of-concept CGNS to csv loader
    Multiprocessing doesn't seem to work with pvpython.

---
+++ Preprocess/generateCSVFromCGNS.py
@@ -0,0 +1,60 @@
+from paraview import simple
+import os, os.path
+
+if __name__ == '__main__':
+    directory = 'atrium data part 2'
+    files = [name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))]
+
+    for file in files:
+        reader = simple.OpenDataFile(directory + "/" + file)
+        reader.PointArrayStatus = ['Temperature']
+        filename = os.path.splitext(file)[0]
+        writer = simple.CreateWriter(directory + "/output/" + filename + ".csv", reader)
+        writer.WriteAllTimeSteps = 1
+        writer.FieldAssociation = "Points"
+        writer.UpdatePipeline()
+        simple.Delete() #Avoids memory leak
+        print "File processed: " + file
+        
+    
+#Multprocessing version (doesn't seem to work with pvpython):
+'''from paraview import simple
+import os, os.path
+from multiprocessing import Process
+
+def f(files, startI, endI):
+    numberOfFiles = len(files)
+    for i in range(startI, endI):
+        file = files[i]
+        reader = simple.OpenDataFile(directory + "/" + file)
+        reader.PointArrayStatus = ['Temperature']
+        filename = os.path.splitext(file)[0]
+        writer = simple.CreateWriter(directory + "/output/" + filename + ".csv", reader)
+        writer.WriteAllTimeSteps = 1
+        writer.FieldAssociation = "Points"
+        writer.UpdatePipeline()
+        simple.Delete() #Avoids memory leak
+        print "File processed: " + file
+    
+if __name__ == '__main__':
+    directory = 'atrium data part 2'
+    files = [name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))]
+    
+
+    p1 = Process(target=f, args=(files, 0, 20))
+    p1.start()
+    
+    p2 = Process(target=f, args=(files, 20, 40))
+    p2.start()
+    
+    p3 = Process(target=f, args=(files, 40, 60))
+    p3.start()
+    
+    p4 = Process(target=f, args=(files, 80, 100))
+    p4.start()
+    
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+'''
