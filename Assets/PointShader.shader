6622c7495ffcd6456e87375a730e243e81ae35ec
Mads Ronnow <roennowmads@gmail.com>
2017-05-30 17:43:48 +0200

    Added Compute and visualization shader for reduced model

---
+++ Assets/PointShader.shader
@@ -0,0 +1,68 @@
+﻿Shader "Unlit/PointShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			#pragma target es3.1
+
+			RWStructuredBuffer<float4> _Points;
+
+			uniform matrix model;
+			uniform float4 trans;
+			uniform float aspect;
+			uniform int _PointsCount;
+			uniform uint _FrameTime;
+			uniform uint _Magnitude;
+			uniform int _TextureSwitchFrameNumber;
+
+			struct appdata
+			{
+				uint id : SV_VertexID;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				fixed2 color : COLOR;
+			};
+
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+
+				uint quadId = v.id;
+
+				float4 particlePos = -float4(_Points[quadId].xy, 0.0, 1.0);
+
+				o.vertex = mul(model, particlePos);
+				o.vertex += trans;
+				o.vertex = UnityWorldToClipPos(o.vertex.xyz);
+
+				o.color = (_Points[quadId].zw + 1.0) * 0.5;
+
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = fixed4(i.color, 1.0, 1.0);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/ReducedModel.cs
@@ -0,0 +1,330 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Text;
+using System.IO;
+using UnityEngine.Rendering;
+using System.Runtime.InteropServices;
+using System;
+
+public class ReducedModel : MonoBehaviour {
+    public string m_valueDataPath = "OilRigData";
+    public int m_lastFrameIndex = 25;
+    
+    public float m_frameSpeed = 5.0f;
+    public int m_textureSideSizePower = 14;
+
+    private int m_pointsCount = 61440;
+    private Renderer pointRenderer;
+
+    private int m_textureSideSize;
+    private int m_textureSize;
+    private int m_lookupTextureSize = 256; //Since the values in the value texture are only 0-255, it doesn't make sense to have more values here.
+
+    private int m_textureSwitchFrameNumber = -1;
+
+    private ComputeBuffer computebuffer;
+
+    public ComputeShader computeShader;
+    private int _kernel;
+
+    public int getPointCount () {
+        return m_pointsCount;
+    }
+
+    public void setPointCount (int pointsCount) {
+        m_pointsCount = pointsCount;
+    }
+
+    public void changePointsCount(int increment) {
+        m_pointsCount += increment;
+    }
+
+    Texture2D createColorLookupTexture() {
+        int numberOfValues = m_lookupTextureSize;
+
+        Texture2D lookupTexture = new Texture2D(m_lookupTextureSize, 1, TextureFormat.RGB24, false, false);
+        lookupTexture.filterMode = FilterMode.Point;
+        lookupTexture.anisoLevel = 1;
+
+        for (int i = 0; i < numberOfValues; i++) {
+            float textureIndex = i;
+
+            //0 - 255 --> 0.0 - 1.0
+            float value = textureIndex / numberOfValues;
+
+            var a = (1.0f - value) / 0.25f; //invert and group
+            float X = Mathf.Floor(a);   //this is the integer part
+            float Y = a - X; //fractional part from 0 to 255
+
+            Color color;
+
+            switch ((int)X) {
+                case 0:
+                    color = new Color(1.0f, Y, 0);
+                    break;
+                case 1:
+                    color = new Color((1.0f - Y), 1.0f, 0);
+                    break;
+                case 2:
+                    color = new Color(0, 1.0f, Y);
+                    break;
+                case 3:
+                    color = new Color(0, (1.0f - Y), 1.0f);
+                    break;
+                case 4:
+                    color = new Color(0, 0, 1.0f);
+                    break;
+                default:
+                    color = new Color(1.0f, 0, 0);
+                    break;
+            }            
+            lookupTexture.SetPixel(i, 0, color); 
+
+            //alternatives: (necessary if I want to store only one component per pixel)
+            //tex.LoadRawTextureData()
+            //tex.SetPixels(x, y, width, height, colors.ToArray()); 
+            //pixels are stored in rectangle blocks... maybe it would actually be better for caching anyway? problem is a frame's colors would need to fit in a rectangle.
+        }
+
+        lookupTexture.Apply();
+
+        return lookupTexture;
+    }
+
+    IEnumerator readPointsFile1ValueAsync(int k, Texture2D tex2) {
+        byte[] vals = new byte[m_textureSize];
+
+        int index = 0;
+        for (int i = k + 1; i < m_lastFrameIndex; i++) {
+
+            TextAsset ta = Resources.Load(m_valueDataPath + "/frame" + i + "0.0", typeof(TextAsset)) as TextAsset; //LoadAsync
+            byte[] bytes = ta.bytes;
+
+            int frameSize = bytes.Length;
+
+            Buffer.BlockCopy(bytes, 0, vals, index * frameSize, frameSize);
+            index++;
+
+        }
+        tex2.LoadRawTextureData(vals);
+        tex2.Apply();
+
+        //pointRenderer = GetComponent<Renderer>();
+        pointRenderer.material.SetTexture("_MainTex2", tex2);
+        yield return 0;
+    }
+
+    void readPointsFile1Value(Texture2D tex, Texture2D tex2) {
+        //CompressionHelper.CompressFile(path + "fireAtrium0." + fileIndex + ".bytes", "fireAtrium0." + fileIndex +".lzf");
+        //byte[] bytes = CompressionHelper.DecompressFileToMem(path + "fireAtrium0." + fileIndex +".lzf");
+        //float[] vals = new float[bytes.Length / 4];
+        //Buffer.BlockCopy(bytes, 0, vals, 0, bytes.Length);
+
+        bool loadingFromBytes = true;
+        
+        byte[] vals = new byte[m_textureSize];
+        //byte[] vals2 = new byte[m_textureSize * m_textureSize];
+
+        //int nextTexIndex = 0;
+
+        if (loadingFromBytes) {
+            for (int k = 0; k < m_lastFrameIndex; k++) {
+
+                //http://answers.unity3d.com/questions/759469/why-doesnt-my-asynchronous-loading-work.html
+                //StartCoroutine (LoadFrames ());
+
+                //ResourceRequest bla = Resources.LoadAsync("AtriumData/frame" + k + "0.0", typeof(TextAsset));
+
+                //while (!bla.isDone) {
+                //    yield return 0;
+                //}
+
+                //TextAsset ta = Resources.Load("AtriumData/binaryDataFull/frame" + k + "0.0", typeof(TextAsset)) as TextAsset; //LoadAsync
+                TextAsset ta = Resources.Load(m_valueDataPath + "/frame" + k + "0.0", typeof(TextAsset)) as TextAsset; //LoadAsync
+                byte[] bytes = ta.bytes;
+
+                int frameSize = bytes.Length;
+                //Debug.Log(k * frameSize);
+                /*if (k * frameSize < m_textureSize - frameSize) {
+                    
+                    Buffer.BlockCopy(bytes, 0, vals, k * frameSize, frameSize);
+                    //nextTexIndex = k;
+                }*/
+
+                if (k * frameSize >= m_textureSize - frameSize) {
+                    m_textureSwitchFrameNumber = k - 1;
+                    StartCoroutine(readPointsFile1ValueAsync(k, tex2));
+                    break;
+                }
+                Buffer.BlockCopy(bytes, 0, vals, k * frameSize, frameSize);
+                
+
+
+                //else {
+                //    Debug.Log(k * frameSize - (nextTexIndex + 1) * frameSize);
+                //    Buffer.BlockCopy(bytes, 0, vals2, k * frameSize - (nextTexIndex+1) * frameSize, frameSize);
+                //}
+
+                //Debug.Log(nextTexIndex);
+
+                //times[k] = offset;
+                //offset += m_pointsCount;
+            }
+
+            //CompressionHelper.CompressMemToFile(vals, "Assets/Resources/OilRigData/compressedValues.bytes");
+        }
+        else {
+            TextAsset ta = Resources.Load(m_valueDataPath + "/compressedValues", typeof(TextAsset)) as TextAsset;
+            vals = CompressionHelper.DecompressBytes(ta.bytes);
+
+            for (int k = 0; k < m_lastFrameIndex; k++) {
+                //times[k] = offset;
+                //offset += m_pointsCount;
+            }
+        }
+
+        //We will have to use textures for values because compute buffers require an element size of 4 bytes, hence we can't use bytes.
+        //It could be done with bit shifting, but then any advantage would wear off.
+
+        tex.LoadRawTextureData(vals);
+        tex.Apply();
+        //tex2.LoadRawTextureData(vals2);
+        //tex2.Apply();
+
+
+    } 
+
+    float[] readPointsFile3Attribs()
+    {
+        TextAsset pointData = Resources.Load(m_valueDataPath + "/frame00.0.pos", typeof(TextAsset)) as TextAsset;
+        byte[] bytes = pointData.bytes;
+        float[] points = new float[(bytes.Length / 4)];
+        Buffer.BlockCopy(bytes, 0, points, 0, bytes.Length);
+        return points;
+    }
+
+    void Start () {
+        //m_textureSideSize = 1 << m_textureSideSizePower;
+        //m_textureSize = m_textureSideSize * m_textureSideSize;
+
+        _kernel = computeShader.FindKernel("CSMain");
+
+        int dimensionWidth = 1024;
+        int dimensionHeight = 1024;
+        Vector4[] points = new Vector4[dimensionWidth*dimensionHeight];
+        Vector4 startingPosition = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
+        
+        float deltaPos = 0.005f;
+
+        for (int i = 0; i < dimensionWidth; i++) {
+            for (int j = 0; j < dimensionHeight; j++) {
+                int index = i + j * dimensionWidth;
+                points[index] = new Vector4();
+                //float randomVal = UnityEngine.Random.Range(0.0f, 1.0f);
+                points[index].x = i * deltaPos/* + randomVal*10.0f*/; //- dimensionWidth*deltaPos + dimensionWidth*deltaPos*0.5f + randomVal - 0.5f + startingPosition.x;
+                //randomVal = UnityEngine.Random.Range(0.0f, 1.0f);
+                points[index].y = j * deltaPos/* + randomVal*10.0f*/; //- dimensionHeight*deltaPos + dimensionWidth*deltaPos*0.5f + randomVal - 0.5f + startingPosition.y;
+            }
+        }
+
+
+        //Set up mesh:
+        m_pointsCount = points.Length;
+
+         //Set up textures:
+        /*Texture2D colorTexture = createColorLookupTexture();
+        
+        //We don't need more precision than the resolution of the colorTexture. 10 bits is sufficient for 1024 different color values.
+        //That means we can pack 3 10bit integer values into a pixel 
+        //Texture2D texture = new Texture2D(m_textureSize, m_textureSize, TextureFormat.RFloat, false, false);
+        Texture2D texture = new Texture2D(m_textureSideSize, m_textureSideSize, TextureFormat.Alpha8, false, false);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Repeat;
+
+        Texture2D texture2 = new Texture2D(m_textureSideSize, m_textureSideSize, TextureFormat.Alpha8, false, false);
+        texture2.filterMode = FilterMode.Point;
+        texture2.wrapMode = TextureWrapMode.Repeat;*/
+
+        /*bool supportsTextureFormat = SystemInfo.SupportsTextureFormat(TextureFormat.R16); 
+        if (supportsTextureFormat) {
+            Debug.Log("");
+        }*/
+
+        /*texture.anisoLevel = 1;
+        readPointsFile1Value(texture, texture2);
+        pointRenderer = GetComponent<Renderer>();
+        pointRenderer.material.mainTexture = texture;
+        //pointRenderer.material.SetTexture("_MainTex2", texture2);
+        pointRenderer.material.SetTexture("_ColorTex", colorTexture);*/
+
+        pointRenderer = GetComponent<Renderer>();
+
+        computebuffer = new ComputeBuffer (m_pointsCount, Marshal.SizeOf(typeof(Vector4)), ComputeBufferType.GPUMemory);
+        computebuffer.SetData(points);
+        pointRenderer.material.SetBuffer ("_Points", computebuffer);
+
+        pointRenderer.material.SetInt("_PointsCount", m_pointsCount);
+        float aspect = Camera.main.GetComponent<Camera>().aspect;
+        pointRenderer.material.SetFloat("aspect", aspect);
+        Vector4 trans = transform.position;
+        pointRenderer.material.SetVector("trans", trans);
+        pointRenderer.material.SetInt("_Magnitude", m_textureSideSizePower);
+        pointRenderer.material.SetInt("_TextureSwitchFrameNumber", m_textureSwitchFrameNumber);   
+
+        Debug.Log("Number of points: " + m_pointsCount);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Time.fixedTime);
+
+        int t = ((int)(Time.fixedTime * m_frameSpeed)) % m_lastFrameIndex;
+
+        //t = 29;
+
+        //Debug.Log(count);
+
+        //Debug.Log(t);
+        pointRenderer.material.SetInt("_FrameTime", t);
+        float aspect = Camera.main.GetComponent<Camera>().aspect;
+        pointRenderer.material.SetFloat("aspect", aspect);
+
+        //Debug.Log("Support instancing: " + SystemInfo.supportsInstancing);
+
+        //int a = pointRenderer.material.GetInt("_FrameTime");
+
+        //Debug.Log(a);
+    }
+
+    private void OnRenderObject()
+    {
+        Dispatch();
+
+        pointRenderer.material.SetPass(0);
+        pointRenderer.material.SetMatrix("model", transform.localToWorldMatrix);
+        //Graphics.DrawProcedural(MeshTopology.Points, 1, m_pointsCount);
+        Graphics.DrawProcedural(MeshTopology.Points, m_pointsCount);  // index buffer.
+    }
+
+    /*void OnPostRender ()
+    {
+        Dispatch();
+    }*/
+
+    void OnDestroy() {
+        computebuffer.Release();
+    }
+    private void Dispatch()
+    {
+        //constantBuffer.SetData(new[] { Time.time });
+ 
+        //pointRenderer.material.SetBuffer ("_Points", computebuffer);
+        computeShader.SetBuffer(_kernel, "_Points", computebuffer);
+        //computeShader.SetBuffer(_kernel, "offsets", offsetBuffer);
+        //computeShader.SetBuffer(_kernel, "output", outputBuffer);
+ 
+        computeShader.Dispatch(_kernel, 1024 * 1024, 1, 1);
+    }
+}
